0a90d811620c8376c1518c8eb42bbbb1ef8a158f
Nicolas Carrasco <nicolascarrasco98@hotmail.com>
2020-06-12 21:55:19 +0000

    Configuracion

--- controlador/procesos.py
+++ controlador/procesos.py
@@ -164,8 +164,9 @@ def topicmodeling(n):
   try:
     pyLDAvis.enable_notebook()
   except:
-    vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word,sort_topics=False)
-    pyLDAvis.save_html(vis, 'templates/LDA_Visualization.html')
+    print()
+  vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word,sort_topics=False)
+  pyLDAvis.save_html(vis, 'templates/LDA_Visualization.html')
       
   for i in range(5):
     wordcloud = WordCloud(stopwords=n4,max_font_size=50, max_words=100, background_color="white").generate(tt[i])
