2ec418e0d82c5f9403068305d68d0e83e4798c7c
Nicolas Carrasco <nicolascarrasco98@hotmail.com>
2020-06-13 06:11:57 +0000

    Arreglo LDA

--- controlador/procesos.py
+++ controlador/procesos.py
@@ -110,6 +110,7 @@ def topicmodeling(n):
   from wordcloud import WordCloud
 
   tpm = []
+  #stopwords
   n4 = stopwords.words('spanish')
   n4 += stopwords.words('english')
   n4.append('gt')
@@ -129,8 +130,8 @@ def topicmodeling(n):
   id2word = corpora.Dictionary(tweet)
   #Bolsa de palabras
   corpus = [id2word.doc2bow(text) for text in tweet]
-
-  lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word,num_topics=4,alpha='auto')
+  #algoritmo lda_model
+  lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word,num_topics=4,random_state=100,update_every=1,chunksize=100,passes=10,alpha='auto',per_word_topics=True)
 
   topics = []
   for idx, topic in lda_model.print_topics(-1):
